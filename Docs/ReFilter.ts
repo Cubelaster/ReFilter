1471b94e2e309978a015fc70425ddd061dad5686
Cubelaster <david.cubela@gmail.com>
2021-02-08 15:59:42 +0100

    FE Models

---
+++ Docs/ReFilter.ts
@@ -0,0 +1,51 @@
+export interface PagedBase {
+    pageSize: number;
+    pageIndex: number;
+}
+
+export enum SortDirection {
+    ASC = 0,
+    DESC = 1
+}
+
+export enum OperatorComparer {
+    Contains,
+    StartsWith,
+    EndsWith,
+    Equals = 13,
+    GreaterThan = 15,
+    GreaterThanOrEqual = 16,
+    LessThan = 20,
+    LessThanOrEqual = 21,
+    NotEqual = 35,
+    Not = 34,
+    CustomFilter = 99
+}
+
+export interface PropertyFilterConfig {
+    propertyName: string;
+    operatorComparer?: OperatorComparer | null;
+    sortDirection?: SortDirection | null;
+    value?: any;
+}
+
+export interface BasePagedRequest extends PagedBase {
+    /**
+     * Where object for 1:1 mapping to entity to be filtered.
+     * Only requirenment is that property names are same
+     */
+    where: any;
+
+    /**
+     * Defines rules for sorting and filtering
+     * Can be left empty and in such way, the default values are used.
+     * Default values are no sort and Equals comparer
+     */
+    propertyFilterConfigs?: PropertyFilterConfig[];
+
+    /**Dictionary containing Keys matching PropertyNames and Value matching SortDirection */
+    sorting?: { [key: string]: SortDirection };
+
+    /**String SearchQuery meant for searching ANY of the tagged property */
+    searchQuery?: string;
+}
