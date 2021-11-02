4b01ef27b725907072628a2a2cf9fc1fab7b81ae
Cubelaster <david_cubela@hotmail.com>
2021-11-02 10:10:17 +0100

    Changes to IReSort interface. Added SortDirection, since it's necessary.
    First Test ReSortConfigBuilder implementation

--- ReFilter/Models/Filtering/Contracts/IReSort.cs
+++ ReFilter/Models/Filtering/Contracts/IReSort.cs
@@ -1,9 +1,10 @@
 ﻿using System.Linq;
+using ReFilter.Enums;
 
 namespace ReFilter.Models.Filtering.Contracts
 {
     public interface IReSort<T> where T : class, new()
     {
-        IOrderedQueryable<T> SortQuery(IQueryable<T> query, bool isFirst = true);
+        IOrderedQueryable<T> SortQuery(IQueryable<T> query, SortDirection sortDirection = SortDirection.ASC, bool isFirst = true);
     }
 }
--- TestProject/RequiredImplementations/ReSortConfigBuilder.cs
+++ TestProject/RequiredImplementations/ReSortConfigBuilder.cs
@@ -3,6 +3,7 @@ using ReFilter.ReFilterConfigBuilder;
 using ReFilter.ReSortBuilder;
 using TestProject.Models;
 using TestProject.Models.FilterRequests;
+using TestProject.SortBuilders;
 
 namespace TestProject.RequiredImplementations
 {
@@ -10,7 +11,13 @@ namespace TestProject.RequiredImplementations
     {
         public IReSortBuilder<T> GetMatchingSortBuilder<T>() where T : class, new()
         {
-            return null;
+            switch (typeof(T))
+            {
+                case Type schoolType when schoolType == typeof(School):
+                    return (IReSortBuilder<T>)new SchoolSortBuilder();
+                default:
+                    return null;
+            }
         }
 
         public Type GetMatchingType<T>() where T : class, new()
