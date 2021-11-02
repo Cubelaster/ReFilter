9181f03fc9045c3dbba7abc1f8e3a61d958f8c29
Cubelaster <david.cubela@petrolsofting.com>
2021-11-02 11:46:16 +0100

    Fix and Version Increment

--- ReFilter/Extensions/IServiceCollectionExtension.cs
+++ ReFilter/Extensions/IServiceCollectionExtension.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using Microsoft.Extensions.DependencyInjection;
 using ReFilter.ReFilterActions;
+using ReFilter.ReFilterConfigBuilder;
 using ReFilter.ReFilterTypeMatcher;
 
 namespace ReFilter.Extensions
 {
     public static class IServiceCollectionExtension
     {
-        public static IServiceCollection AddReFilter(this IServiceCollection services, Type ReFilterTypeMatcherImplementation)
+        public static IServiceCollection AddReFilter(this IServiceCollection services, 
+            Type ReFilterTypeMatcherImplementation,
+            Type ReSortTypeMatcherImplementation)
         {
             services.AddScoped<IReFilterActions, ReFilterActions.ReFilterActions>();
             services.AddScoped(typeof(IReFilterConfigBuilder), ReFilterTypeMatcherImplementation);
+            services.AddScoped(typeof(IReSortConfigBuilder), ReSortTypeMatcherImplementation);
 
             return services;
         }
