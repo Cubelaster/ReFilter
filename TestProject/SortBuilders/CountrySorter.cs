6b8bb070983c520d26c550808db7e3a54fd9f8ef
PETROLSOFTING\david.cubela <david.cubela@petrolsofting.com>
2026-09-10 10:09:22 +0200

    a Sorter fix

--- TestProject/SortBuilders/CountrySorter.cs
+++ TestProject/SortBuilders/CountrySorter.cs
@@ -9,13 +9,27 @@ namespace TestProject.SortBuilders
     {
         public IOrderedQueryable<School> SortQuery(IQueryable<School> query, SortDirection sortDirection, bool isFirst = true)
         {
-            if (sortDirection == SortDirection.ASC)
+            if (isFirst)
             {
-                return query.OrderBy(e => (e.Country == null ? null : e.Country.Alpha2Code)).ThenBy(e => e.Address);
+                if (sortDirection == SortDirection.ASC)
+                {
+                    return query.OrderBy(e => (e.Country == null ? null : e.Country.Alpha2Code)).ThenBy(e => e.Address);
+                }
+                else
+                {
+                    return query.OrderByDescending(e => (e.Country == null ? null : e.Country.Alpha2Code)).ThenByDescending(e => e.Address);
+                }
             }
             else
             {
-                return query.OrderByDescending(e => (e.Country == null ? null : e.Country.Alpha2Code)).ThenByDescending(e => e.Address);
+                if (sortDirection == SortDirection.ASC)
+                {
+                    return ((IOrderedQueryable<School>)query).ThenBy(e => (e.Country == null ? null : e.Country.Alpha2Code)).ThenBy(e => e.Address);
+                }
+                else
+                {
+                    return ((IOrderedQueryable<School>)query).ThenByDescending(e => (e.Country == null ? null : e.Country.Alpha2Code)).ThenByDescending(e => e.Address);
+                }
             }
         }
     }
